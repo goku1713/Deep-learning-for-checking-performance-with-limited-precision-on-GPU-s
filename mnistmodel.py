edb204d4073f9f765f6be8c1ead85c3c3be65ac0
goku1713 <prateekparab@gmail.com>
2018-01-24 11:18:46 -0600

    Deep learning

---
+++ mnistmodel.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 20:17:57 2017
+
+@author: Quantum
+"""
+
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+import time
+from matplotlib import pyplot as pt
+data=[]
+time1=[]
+cnt=0
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+while cnt<5:
+    st=time.time()
+    
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+    
+    # the data, shuffled and split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+    
+    x_train = x_train.astype('float64')
+    x_test = x_test.astype('float64')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+    
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    y_train = y_train.astype('float64')
+    y_test = y_test.astype('float64')
+    
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    et=time.time()
+    tt=et-st
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    data.append(score[1])
+    time1.append(tt)
+    cnt+=1
+a=np.array(data)
+b=np.array(time1)
+pt.xlabel('Accuracy')
+pt.ylabel('Time')
+pt.scatter(a,b)
